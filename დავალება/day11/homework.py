65f2f93888b9dbd992c6284e01cd42946ad3d3d5
RobaAmonashvili1 <138668133+RobaAmonashvili1@users.noreply.github.com>
2023-08-18 14:01:30 +0400

    Delete დავალება directory

--- დავალება/day11/homework.py
+++
@@ -1,9 +0,0 @@
-for number in range(1, 101):
-    if number % 15 == 0:
-        print("GOA15")
-    elif number % 5 == 0:
-        print("GOA11")
-    elif number % 3 == 0:
-        print("GOA")
-    else:
-        print(number)
--- დავალება/day3/day3.py
+++
@@ -1,7 +0,0 @@
-name = "რობა"
-surname = "ამონაშვილი"
-age = 13
-height = 1.49
-country = "საქართველო"
-city = "თბილისი"
-print(name + " " + surname + " " + str(age) + " " + str(height) + " " + country + " " + city)
