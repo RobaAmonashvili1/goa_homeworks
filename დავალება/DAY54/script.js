70e21de73640cf21c32c036e84fd5b13f7950dcd
RobaAmonashvili1 <138668133+RobaAmonashvili1@users.noreply.github.com>
2024-01-26 18:15:44 +0400

    Add files via upload

---
+++ დავალება/DAY54/script.js
@@ -0,0 +1,18 @@
+let num1 = prompt('enter any number')
+let num2 = prompt('enter second number')
+let age1 = '14'
+let age2 = 14
+let x = 5
+let y = 6
+console.log(x < y || y > x)
+console.log(x < y && y < x)
+console.log(Number(age1))
+console.log(String(age2))
+console.log(Number(num1) + Number(num2))
+
+let me = {
+    name:'Roba',
+    surname:'Amonashvili',
+    age:14,
+}
+console.log(me)
